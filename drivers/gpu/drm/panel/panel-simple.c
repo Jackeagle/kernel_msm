a698c1035cc80f6600af57b7d49cb1f0062bfcff
Stephen Boyd <swboyd@chromium.org>
2020-11-09 16:36:14 -0800

    CHROMIUM: drm/panel: simple: Add flags to boe_nv110wtm_n61
    Reading the EDID of this panel shows that these flags should be
    set. Set them so that we match what is in the EDID.
    
    BUG=b:169288627, b:172678127
    TEST=Boot on CoachZ, don't see flash for login
    
    Signed-off-by: Stephen Boyd <swboyd@chromium.org>
    Change-Id: Idbcddf488bc280b7561d7b3a66483ab237aa78aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2527952
    Reviewed-by: Douglas Anderson <dianders@chromium.org>

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -1404,6 +1404,7 @@ static const struct panel_desc boe_nv101wxmn51 = {
 static const struct drm_display_mode boe_nv110wtm_n61_modes[] = {
 	{
 		.clock = 207800,
+		.flags = DRM_MODE_FLAG_PHSYNC | DRM_MODE_FLAG_NVSYNC,
 		.hdisplay = 2160,
 		.hsync_start = 2160 + 48,
 		.hsync_end = 2160 + 48 + 32,
@@ -1415,6 +1416,7 @@ static const struct drm_display_mode boe_nv110wtm_n61_modes[] = {
 	},
 	{
 		.clock = 138500,
+		.flags = DRM_MODE_FLAG_PHSYNC | DRM_MODE_FLAG_NVSYNC,
 		.hdisplay = 2160,
 		.hsync_start = 2160 + 48,
 		.hsync_end = 2160 + 48 + 32,
